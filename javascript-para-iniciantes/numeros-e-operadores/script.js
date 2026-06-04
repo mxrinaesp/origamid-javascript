b7e232fdff8ce4f6445df26eb3169c86f59f7cc4
Marina <marinaesp08@gmail.com>
2026-06-04 20:35:20 +0200

    4. Números e Operadores 2: operadores unários de incremento e +|-

--- javascript-para-iniciantes/numeros-e-operadores/script.js
+++ javascript-para-iniciantes/numeros-e-operadores/script.js
@@ -20,5 +20,36 @@ console.log(modulo);
 var soma = '100' + 50; //10050 porque concatena
 var subtracao = '100' - 50; // 50 correcto
 var multiplicacao = '100' * '2'; // 200 correcto
-var divisao = 'Comprei 10' / 2; // ERROR NaN (Not a Number) pero si '100', 5 correcto
+var divisao = 'Comprei 10' / 2; // ERROR NaN (Not a Number) pero si '10', 5 correcto
 console.log(divisao);
+
+
+// Operadores aritméticos unários I
+var incremento = 5;
+console.log(incremento++); // 5 + 1 y guarda el valor ... misma cosa con x--
+console.log(incremento) // 6 porque muestra el resultado;
+
+var incremento2 = 10;
+console.log(++incremento2); // 11 porque la suma va delante ... misma cosa con --x
+console.log(incremento2); // 11 porque muestra el resultado
+
+
+// Operadores aritméticos unários II
+var ano = '2020';
+console.log(-ano +5); // -2015 ... +|- transforma el valor del String en número 
+console.log(+ano +5); // 2025
+
+var frase = 'Isso é um teste';
+console.log(+frase); // NaN 
+console.log(-frase); // NaN
+
+var possuiFaculdade = true;
+console.log(+possuiFaculdade); // 1
+
+
+// Como dividir o peso por 2?
+var numero ='80'; // +'80' / 2
+var unidade = 'kg';
+var peso = numero + unidade; //
+var pesoPorDois = peso / 2; // quitamos el '/ 2' porque ya hicimos división arriba"
+console.log(pesoPorDois);
